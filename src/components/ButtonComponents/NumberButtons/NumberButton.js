9c455c0b0c7d73d73c2c03a62e8372018186d456
Jonathan Scott <jonathan.scott@gmail.com>
2019-07-16 12:07:07 -0700

    yarn install and basic exports set up

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,6 +1,7 @@
 import React from "react";
+import {numbers} from '../../../data'
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
